2a64ddd80ade627f344efbe2aacc4788b32fa688
PsvitaNeedsCat <53197859+PsvitaNeedsCat@users.noreply.github.com>
2021-04-24 16:58:10 +1200

    Main UI
    - Canvas has been prefabed
    - UI added for items and money

--- Assets/Scripts/PlayerInventory.cs
+++ Assets/Scripts/PlayerInventory.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerInventory : MonoBehaviour
 {
@@ -9,6 +10,8 @@ public class PlayerInventory : MonoBehaviour
     public float playerCurrency = 0.0f;
 
     private Dictionary<ItemData.Type, int> inventory = new Dictionary<ItemData.Type, int>();
+
+    private int m_totalItems = 0;
 
     private void Awake()
     {
@@ -28,5 +31,12 @@ public class PlayerInventory : MonoBehaviour
         }
 
         inventory[data.type] = inventory[data.type] + amount;
+        m_totalItems += amount;
+
+        InventoryUI.Instance.UpdateItem(data.type, inventory[data.type], m_totalItems);
+
+        // Update currency
+        playerCurrency += data.value * amount;
+        InventoryUI.Instance.UpdateMoney(playerCurrency);
     }
 }
